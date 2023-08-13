e7db0522bc7671da78fa9707c1dc389895b9caf8
anselmo <anselmostitla@gmail.com>
2023-08-12 20:17:08 -0600

    sendisure

--- context/account.jsx
+++ context/account.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { createContext, useContext, useEffect, useState } from "react"
 import { ethers } from 'ethers'
-    
 
 
 const accountContext = createContext()
@@ -12,7 +11,7 @@ export const useAccount = () => {
 export const AccountProvider = ({children}) => {
   const [account, setAccount] = useState()
   const [network, setNetwork] = useState()
-  
+  // const []
 
   const connectWallet = async () => {
     if(!window.ethereum) return
